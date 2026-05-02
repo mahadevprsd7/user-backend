e3a690fdd98ee22fd82a9b55c323ae03222512c5
mahadevprsd7 <mahadevprsd7@gmail.com>
2026-05-02 10:46:51 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(5000, () => console.log("Server running"));
